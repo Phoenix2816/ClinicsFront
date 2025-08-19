90837ddd97219f2bd3f9a5f9becb6f870df938d5
Phoenix2816 <145472889+Phoenix2816@users.noreply.github.com>
2025-08-19 21:56:00 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+
+module.exports = {
+
+    devServer: {
+        proxy: {
+            'api': {
+                target: 'https://clinics-7.onrender.com',
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
+}
