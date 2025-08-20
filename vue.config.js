629fe3c90ea144fbf40e1ac681376b03aa49bdc6
Phoenix2816 <145472889+Phoenix2816@users.noreply.github.com>
2025-08-20 18:09:15 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,12 @@ module.exports = defineConfig({
     transpileDependencies: true
 })
 
+// vue.config.js
 module.exports = {
   transpileDependencies: true,
   devServer: {
     host: '0.0.0.0',
-    port: 8080, 
+    port: 8080,
     allowedHosts: 'all',
     proxy: {
       '/api': {
@@ -19,3 +20,4 @@ module.exports = {
     }
   }
 }
+
