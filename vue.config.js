697de7ee2b35230a00c9c5a56b44b7cbcfe20211
Phoenix2816 <145472889+Phoenix2816@users.noreply.github.com>
2025-08-30 15:47:53 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-<<<<<<< HEAD
-    transpileDependencies: true
-})
-
-// vue.config.js
-module.exports = {
   transpileDependencies: true,
   devServer: {
     host: '0.0.0.0',
     port: 8080,
-=======
-  transpileDependencies: true,
-  devServer: {
->>>>>>> d8bc5ef (Initial commit)
     allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'https://clinics-7.onrender.com',
         changeOrigin: true,
         secure: false,
-<<<<<<< HEAD
         pathRewrite: { '^/api': '/api' }
       }
     }
   }
-}
-
-=======
-      }
-    }
-  }
 })
->>>>>>> d8bc5ef (Initial commit)
